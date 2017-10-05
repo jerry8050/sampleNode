ab36625cdb9a707dbb18fee0cdbf4350b3c38782
Jacob, Jerry <Jerry.Jacob@move.com>
2017-10-04 19:21:20 -0700

    Proc

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'rdc-hackathon.c3d8n8cosrif.us-west-2.rds.amazonaws.com',
-    user     : 'root',
-    password : 'realtor.com',
-    database : 'ads'
+    host:     'rdc-hackathon.c3d8n8cosrif.us-west-2.rds.amazonaws.com',
+    user:     'root',
+    password: 'realtor.com',
+    database: 'ads'
 });
 
 connection.connect();
 
 /* GET home page. */
-router.get('/:id', function(req, res){
-  console.log(req.params.id);
+router.get('/:id', function (req, res) {
+    console.log(req.params.id);
 
     connection.query('SELECT * from ads', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
+
+        if (error) {
+            return res.send("Oh,oh");
+        } else
+            res.send(results);
+    })
+    router.get('/', function (req, res, next) {
+        res.render('index', {title: 'Express'});
     });
-
-    res.send("ok");
-})
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
 });
-
-module.exports = router;
+    module.exports = router;
